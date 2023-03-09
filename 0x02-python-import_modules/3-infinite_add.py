0c826d74e0e8b7c4e469e3b78f1cb484b8ada490
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-03-09 16:44:24 +0300

    my last commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 3-infinite_add.py
-# Brennan D Baraban <375@holbertonschool.com>
+# wakjira Bekele <wakjirabekele2018@gmail.com>
 
 if __name__ == "__main__":
     """Print the addition of all arguments."""
