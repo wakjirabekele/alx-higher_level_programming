328a3bdb142828a3aa656670769df3d092da70fa
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-03-11 14:04:17 +0300

    my last commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,13 +2,6 @@
 # 6-print_matrix_integer.py
 # wakjira Bekele <wakjirabekele2018@gmail.com>
 
-
 def print_matrix_integer(matrix=[[]]):
-    """Print a matrix of integers."""
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
-
-        print("")
+    for i in matrix:
+        print(' '.join("{:d}".format(j) for j in i))
