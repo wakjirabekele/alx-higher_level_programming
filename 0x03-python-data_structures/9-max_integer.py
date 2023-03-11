1c48ac61cedc33b9fca7c9a47e28e9fab85c31ce
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-03-11 14:08:04 +0300

    my last commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # 9-max_integer.py
-# Brennan D Baraban <375@holbertonschool.com>
-
+# Wakjira Bekele <wakjirabekele2018@gmail.com>
 
 def max_integer(my_list=[]):
     """Find the biggest integer of a list."""
