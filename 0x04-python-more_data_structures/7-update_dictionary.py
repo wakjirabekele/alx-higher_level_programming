2cd8e49d6e0beccabd5b925f6d9218b88ff8bb8e
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-03-15 16:00:05 +0300

    my last commit

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,8 +2,6 @@
 # 7-update_dictionary.py
 # wakjira Bekele <wakjirabekele2018@gmail.com>
 
-
 def update_dictionary(a_dictionary, key, value):
-    """Replace or add key/value pairs in a dictionary."""
     a_dictionary[key] = value
-    return (a_dictionary)
+    return a_dictionary
