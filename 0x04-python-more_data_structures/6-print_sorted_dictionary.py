f6462224a62cda44277c686f3eff8d35bb04e71b
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-03-15 15:46:40 +0300

    my last commit

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 6-print_sorted_dictionary.py
-# Brennan D Baraban <375@holbertonschool.com>
+# wakjira Bekele <wakjirabekele2018@gmail.com>
 
 
 def print_sorted_dictionary(a_dictionary):
