b71af4bb2816aec55a2e45763e6ba71864e61bd8
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-03-15 15:47:13 +0300

    my last commit

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 7-update_dictionary.py
-# Brennan D Baraban <375@holbertonschool.com>
+# wakjira Bekele <wakjirabekele2018@gmail.com>
 
 
 def update_dictionary(a_dictionary, key, value):
