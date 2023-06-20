144ba914ff341d73d1d6359c47f818549020e9c5
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-06-20 12:01:12 +0300

    Update 0-select_states.sql

--- 0x0F-python-object_relational_mapping/tests/0-select_states.sql
+++ 0x0F-python-object_relational_mapping/tests/0-select_states.sql
@@ -8,4 +8,8 @@ CREATE TABLE IF NOT EXISTS states (
   PRIMARY KEY (id)
 );
 
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+INSERT INTO states (name) VALUES ("California");
+INSERT INTO states (name) VALUES ("Arizona");
+INSERT INTO states (name) VALUES ("Texas");
+INSERT INTO states (name) VALUES ("New York");
+INSERT INTO states (name) VALUES ("Nevada");
