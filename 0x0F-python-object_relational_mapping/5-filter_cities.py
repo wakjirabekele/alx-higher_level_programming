cb59627f7ec4f56087869ca9e935493e536bb9f8
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-06-21 14:04:09 +0300

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-# Displays all cities of a given state from the
-# states table of the database hbtn_0e_4_usa.
-# Safe from SQL injections.
-# Usage: ./5-filter_cities.py <mysql username> \
-#                             <mysql password> \
-#                             <database name> \
-#                             <state name searched>
-import sys
-import MySQLdb
+"""Script takes state name as an argument and lists all cities of given state
+Takes three arguments:
+    mysql username
+    mysql password
+    database name
+Connects to default host (localhost) and port (3306)
+"""
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+    param = (argv[4], )
+    c.execute("SELECT * FROM cities\
+            INNER JOIN states ON cities.state_id = states.id\
+            WHERE states.name = %s\
+            ORDER BY cities.id ASC", param)
+    rows = c.fetchall()
+    cities = []
+    for row in rows:
+        if row[4] == param[0]:
+            cities.append(row[2])
+    print(', '.join(cities))
+    c.close()
+    db.close()
