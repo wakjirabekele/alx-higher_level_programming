4d38a6c9a3a7dab18dda38b6d843583b0db54b1b
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-06-21 13:58:47 +0300

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Safe from SQL injections.
-# Usage: ./3-my_safe_filter_states.py <mysql username> \
-#                                     <mysql password> \
-#                                     <database name> \
-#                                     <state name searched>
-import sys
-import MySQLdb
+"""Script takes in arguments, sanitizes them, and display state
+Takes four arguments:
+    mysql username
+    mysql password
+    database name
+    name to match (to sanitize)
+Connects to default host (localhost) and port (3306)
+"""
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+    param = (argv[4], )
+    c.execute("SELECT * FROM states WHERE name = %s\
+            ORDER BY states.id ASC", param)
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
